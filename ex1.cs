c4a4092ac48fb8cf62e21e6cb3cafb7c01ff4d83
nooty <annsk5342@gmail.com>
2025-03-10 18:55:21 +0900

    datatype

--- ex1.cs
+++ ex1.cs
@@ -1,4 +1,10 @@
 using System;
+// using : 사용하겠다.
+/* System : C# 코드가 기본적으로 필요로 하는 클래스를 담고있음
+    즉, namespace안에 있는 클래스를 사용하겠다고 컴파일러에게 알리는 역할
+    
+    -컴파일러 : 프로그래밍 언어를 기계어로 변환하는 소프트웨어 도구
+*/
 
 class ex1{
     public static void Main(){
@@ -10,7 +16,7 @@ class ex1{
         // 추가
         Console.WriteLine("Hellow world");
         Console.WriteLine("Hellow c#");
-
+        Console.WriteLine(System.DateTime.Now);
 
         for(int i = 0; i < 5; i++){
             Console.WriteLine(i);
---
+++ ex_data.cs
@@ -0,0 +1,37 @@
+using System;
+
+class ex_interger(){
+
+    byte a;
+    sbyte sa;
+    short b;
+    ushort ub;
+    int c;
+    uint uc;
+    long d;
+    ulong ud;
+    float e;
+    double f;
+    decimal g;
+
+    int i = int.MaxValue;
+    float l = float.MinValue;
+
+}
+
+class ex_boolean(){
+
+bool b = true;
+
+
+}
+
+class ex_string(){
+    char a;
+    string b;
+    
+}
+
+class ex_object(){
+    object a;
+}
