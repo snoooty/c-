7ce2946b847ae10cd0cac8b1707fef8c44e582a2
nooty <annsk5342@gmail.com>
2025-03-10 18:03:52 +0900

    first commit

---
+++ ex1.cs
@@ -0,0 +1,18 @@
+using System;
+
+class ex1{
+    public static void Main(){
+        // 코멘트는 이렇게 작성
+        /* 복수 작성할때는 이렇게
+
+        할수있다.
+        */
+        Console.WriteLine("Hellow world");
+        Console.WriteLine("Hellow c#");
+
+
+        for(int i = 0; i < 5; i++){
+            Console.WriteLine(i);
+        }
+    }
+}
